3379550f2d781caa5c9026b447c53a0b04a134b5
BrunoHolmes <52051840+BrunoHolmes@users.noreply.github.com>
2019-10-18 17:20:22 -0300

    Add files via upload
    Resolvendo a seguinte questão: "Chico tem 1.10, Juca tem 1.20. Sabendo que a cada ano que passa chico cresce 2
    cm e Juca cresce 1 cm, quando anos são necessários para chico ser mais alto que JUCA?"

---
+++ ChicoEJuca.java
@@ -0,0 +1,22 @@
+
+public class lista2exercicio11 {
+
+	public static void main(String[] args) {
+		final double CRESCIMENTO_POR_ANO_CHICO = 0.2;
+		final double CRESCIMENTO_POR_ANO_JUCA = 0.1;
+		
+		double chico = 1.10;
+		double juca = 1.20;
+		
+		int contador = 0;
+			do {
+				chico = chico + CRESCIMENTO_POR_ANO_CHICO;
+				juca = juca + CRESCIMENTO_POR_ANO_JUCA;
+				contador++;}
+			
+			while(chico<=juca); 
+				
+				System.out.println("Chico agora é maior que Juca, sua altura é: " + chico + ", enquanto a de Juca é: " + juca );
+				System.out.println("Foram necessários " + contador + " anos para ele ultrapassá-lo.");
+	}
+}
